8864da1777194f71e100569b24e6431a40729a84
Matthew Thompson <mthompson@smashfly.com>
2020-02-20 23:05:08 +0000

    Update Heading if current hour is 15

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+const TIMEHEADER = document.querySelector("#timeHeader");
 
 //Initial Time values
 var date = new Date();
@@ -13,7 +14,7 @@ let hourPos = (hr * 360/12) + (min * (360/60)/12);
 let minPos = (min * 360/60) + (sec * (360/60)/60);
 let secPos = sec* 360/60;    
 
-function initClock(){
+function tick(){
 
 
     //Increment position by degree
@@ -24,7 +25,17 @@ function initClock(){
     //Set hand position
     HOURHAND.style.transform = "rotate(" + hourPos + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPos + "deg)";
-    SECONDHAND.style.transform = "rotate(" + secPos + "deg)";    
+    SECONDHAND.style.transform = "rotate(" + secPos + "deg)";
+    
+    //If current hour is 15, then display Tea at 3 text
+    if (date.getHours() == 15){
+
+        TIMEHEADER.innerText = "TIME FOR TEA FAM!";
+    }
+    else{
+        TIMEHEADER.innerHTML = "Not there yet sorry."
+    }    
+    
 }
 
-setInterval(initClock, 1000);
+setInterval(tick, 1000);
